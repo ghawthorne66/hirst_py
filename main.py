31a10dd7389c9d493bb0a0eba2039ed6528fa3c9
Greg Hawthorne <ghawthorne@me.com>
2020-11-19 11:53:44 +0100

    add colors.py

--- main.py
+++ main.py
@@ -1,8 +1,29 @@
-import colorgram
+import turtle as turtle_module
+import random
+from colors import color_list
 
-rgb_colors = []
-colors = colorgram.extract('image.jpg', 30)
-for color in colors:
-    rgb_colors.append(color.rgb)
+turtle_module.colormode(255)
+tim = turtle_module.Turtle()
+tim.speed("fastest")
+tim.penup()
 
-print(rgb_colors)
+tim.setheading(225)
+tim.forward(300)
+tim.setheading(0)
+number_of_dots = 100
+
+
+for dot_count in range(1, number_of_dots):
+    tim.dot(20, random.choice(color_list))
+    tim.forward(50)
+    if dot_count % 10 == 0:
+        tim.setheading(90)
+        tim.forward(50)
+        tim.setheading(180)
+        tim.forward(500)
+        tim.setheading(0)
+
+
+screen = turtle_module.Screen()
+screen.exitonclick()
+
